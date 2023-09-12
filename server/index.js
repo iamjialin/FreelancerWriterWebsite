d601a9b30116ae8fe45a3d578347dd383f8e1268
Charlie Yuan <brookisnothere@outlook.com>
2023-09-12 17:23:27 +1000

    Updated server configuration to serve files from dist directory

--- server/index.js
+++ server/index.js
@@ -27,10 +27,10 @@ app.use(cors({
 }));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
   }
   

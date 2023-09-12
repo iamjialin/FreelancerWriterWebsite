c4930ac0804cce127463961b2761239bb293174f
Charlie Yuan <brookisnothere@outlook.com>
2023-09-12 15:11:05 +1000

    update

--- server/index.js
+++ server/index.js
@@ -103,3 +103,8 @@ app.post('/send-email', async (req, res) => {
         res.send({ status: 'error', message: error.message });
     }
 });
+
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
